8316c9cc988a841748d6ecffd5e3695c96a05127
Angus Lyall <16108@jpc.school.nz>
2022-03-22 09:42:44 +1300

    v2-Collecting-Storeing-Details

--- BobsBuildingSupplys/Form1.vb
+++ BobsBuildingSupplys/Form1.vb
@@ -1,14 +1,21 @@
 ﻿Public Class CustomerDetails
+    Public CusFirstName As String
+    Public CusLastName As String
+    Public CusBusinessName As String
+    Public CusPhoneNumber As String
+    Public CusBillAdress As String
+    Public CusDeliveryAdress As String
+    Public cusTrade As Boolean = False
+    Public cusAdressSame As Boolean = False
     Private Sub lbl_CusTitle_Click(sender As Object, e As EventArgs) Handles lbl_CusTitle.Click
 
     End Sub
 
     Private Sub CustomerDetails_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txt_CusFirstName.TextChanged
-
+    Private Sub txt_custfirstname_TextChanged(sender As Object, e As EventArgs) Handles txt_CusFirstName.TextChanged
+        CusFirstName = txt_CusFirstName.Text
     End Sub
 
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lbl_custLastName.Click
@@ -20,6 +27,42 @@
     End Sub
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles chk_DeliverySameBilling.CheckedChanged
+        If cusAdressSame = False Then
+            cusAdressSame = True
+        Else
+            cusAdressSame = False
+        End If
+    End Sub
 
+    Private Sub txt_CusLastName_TextChanged(sender As Object, e As EventArgs) Handles txt_CusLastName.TextChanged
+        CusLastName = txt_CusLastName.Text
+    End Sub
+
+    Private Sub txt_CusBusinessName_TextChanged(sender As Object, e As EventArgs) Handles txt_CusBusinessName.TextChanged
+        CusBusinessName = txt_CusBusinessName.Text
+    End Sub
+
+    Private Sub txt_CusPhoneNumber_TextChanged(sender As Object, e As EventArgs) Handles txt_CusPhoneNumber.TextChanged
+        CusPhoneNumber = txt_CusPhoneNumber.Text
+    End Sub
+
+    Private Sub txt_CusBillingAdress_TextChanged(sender As Object, e As EventArgs) Handles txt_CusBillingAdress.TextChanged
+        CusBillAdress = txt_CusBillingAdress.Text
+        If cusAdressSame = True Then
+            CusDeliveryAdress = CusBillAdress
+            txt_CusDeliveryAddress.Text = CusDeliveryAdress
+        End If
+    End Sub
+
+    Private Sub txt_CusDeliveryAddress_TextChanged(sender As Object, e As EventArgs) Handles txt_CusDeliveryAddress.TextChanged
+        CusDeliveryAdress = txt_CusDeliveryAddress.Text
+    End Sub
+
+    Private Sub chk_TradeCus_CheckedChanged(sender As Object, e As EventArgs) Handles chk_TradeCus.CheckedChanged
+        If cusTrade = False Then
+            cusTrade = True
+        Else
+            cusTrade = False
+        End If
     End Sub
 End Class
